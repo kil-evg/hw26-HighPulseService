1b504df9a63b42e6298f67cd8e408b3dfe392000
EKil <loreleyko@gmail.com>
2024-11-04 00:53:45 +0100

    First commit

---
+++ src/main/java/ait/cohort46/HighPulseServiceApplication.java
@@ -0,0 +1,13 @@
+package ait.cohort46;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPulseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighPulseServiceApplication.class, args);
+    }
+
+}
